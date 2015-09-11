c0d96ab5bf26eb32624d9a5b046c415547459a88
玉城 翔 <e095736@tamagusuku-no-MacBook.local>
2015-09-11 17:36:59 +0900

    Modified file csv_read.py

--- csv_read.py
+++ csv_read.py
@@ -3,21 +3,27 @@
 import sys
 import csv
 import codecs
+import numpy as np
 
 #sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
-sys.stdout = codecs.EncodedFile(sys.stdout, 'utf_8')
-sys.stdin = codecs.getreader('utf_8')(sys.stdin)
-
-f = open(sys.argv[1], "rb")
-data_reader = csv.reader(f)
-list = []
-
-for line in data_reader:
-	line = line[0:3] + line[7:9]
-	list.append(line)
-
-f = open('some.csv', 'w')
-writer = csv.writer(f, lineterminator='\n')
-
-writer.writerows(list)
-f.close()
+def nparray_generate(file):
+	sys.stdout = codecs.EncodedFile(sys.stdout, 'utf_8')
+	sys.stdin = codecs.getreader('utf_8')(sys.stdin)
+	
+	f = open(file, "rb")
+	data_reader = csv.reader(f)
+	list = []
+	
+	for line in data_reader:
+		#line = line[0:3] + line[7:9]
+		line = line[1]
+		list.append(line)
+	
+	nparray = np.array(list)
+	return nparray
+	
+	#f = open('some.csv', 'w')
+	#writer = csv.writer(f, lineterminator='\n')
+	#
+	#writer.writerows(list)
+	#f.close()
